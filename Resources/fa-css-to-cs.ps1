b63f472496970f4dcafb706a7c85c2571fe460b0
lyuboasenov <lyubo.asenov@gmail.com>
2018-10-20 20:06:11 +0300

    Add font awsome icons

---
+++ Resources/fa-css-to-cs.ps1
@@ -0,0 +1,22 @@
+﻿# Css taken from fontawsesome.css / web package
+
+# expected format "arrows-alt-h = "\uF337";"
+# output          "public const string ArrowsAltH = "\uF337";"
+
+
+$lines = @()
+$textInfo = (New-Object System.Globalization.CultureInfo @('en-Us', $false) ).TextInfo
+
+foreach($line in Get-Content .\fa.cs) {
+    $nameAndValue = $line.Split('=')
+    $name = $nameAndValue[0].Trim()
+    $value = $nameAndValue[1].Trim()
+
+    $name = $name.Replace('-', ' ')
+    $name = $textInfo.ToTitleCase($name)
+    $name = $name.Replace(' ', '')
+
+    $lines += "public const string $name = $value"
+}
+
+$lines | Set-Content ./fa-fixed.cs
