2934714d1ec9823713e31ef80a979ced02281e9b
lyuboasenov <lyubo.asenov@gmail.com>
2018-11-07 21:15:48 +0200

    environment service added

---
+++ Climbing.Guide/Climbing.Guide.Forms/Services/Impl/Environment.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace Climbing.Guide.Forms.Services {
+   public class Environment : IEnvironment {
+      public string ApplicationDataPath { get; private set; }
+      public string CachePath { get; private set; }
+      public string TempPath { get; private set; }
+
+      public Environment() {
+         ApplicationDataPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+         CachePath = Path.Combine(ApplicationDataPath, "cache");
+         TempPath = Path.GetTempPath();
+
+         Directory.Delete(TempPath, true);
+         Directory.CreateDirectory(TempPath);
+      }
+
+      public string GetTempFileName() {
+         return Path.GetTempFileName();
+      }
+   }
+}
