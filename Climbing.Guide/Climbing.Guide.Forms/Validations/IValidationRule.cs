8ea2e8665a5c9b4c7cb37ea1caba1488df733662
lyuboasenov <lyubo.asenov@gmail.com>
2018-12-03 23:21:02 +0200

    initial validation

---
+++ Climbing.Guide/Climbing.Guide.Forms/Validations/IValidationRule.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Climbing.Guide.Forms.Validations {
+   public interface IValidationRule {
+      void Validate(object value);
+   }
+}
