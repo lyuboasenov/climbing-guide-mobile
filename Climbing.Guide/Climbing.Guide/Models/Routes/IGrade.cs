4898f7b8b531cfc0bd1fc23ea182fb990726c6d8
lyuboasenov <lyubo.asenov@gmail.com>
2018-12-03 23:07:55 +0200

    Remove static grades as they are consumed from the service

--- Climbing.Guide/Climbing.Guide/Models/Routes/IGrade.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Climbing.Guide.Core.Models.Routes {
-   public interface IGrade {
-      int AbsoluteValue { get; }
-      string Name { get; set; }
-   }
-}
--- Climbing.Guide/Climbing.Guide/Models/Routes/IGradeService.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Climbing.Guide.Core.Models.Routes {
-   public interface IGradeService {
-      IGrade GetGrade(int absoluteValue, GradeType gradeType);
-      IEnumerable<IGrade> GetGradeList(GradeType gradeType);
-   }
-}
