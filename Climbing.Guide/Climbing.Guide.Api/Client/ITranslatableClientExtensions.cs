a88cc6f2652ce05446551020ff874f3a54539b8c
lyuboasenov <lyubo.asenov@gmail.com>
2018-12-03 23:10:19 +0200

    move api related stuff to new library

---
+++ Climbing.Guide/Climbing.Guide.Api/Client/ITranslatableClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Climbing.Guide.Api.Client {
+   public static class ITranslatableClientExtensions {
+      public static void SetSelectedLanguage(this ITranslatableClient self, StringBuilder urlBuilder) {
+         if (null == self) {
+            throw new ArgumentNullException(nameof(self));
+         }
+
+         if (null != self.SelectedLanguage &&
+            !(self.SelectedLanguage.Default ?? true)) {
+            urlBuilder.Insert(0, $"{self.SelectedLanguage.Code}/");
+         }
+      }
+   }
+}
