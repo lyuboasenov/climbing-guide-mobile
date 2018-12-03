ecdb20315fc0606e8c0d6f6a424d5f8e29b6ad76
lyuboasenov <lyubo.asenov@gmail.com>
2018-12-03 23:12:41 +0200

    Rename error service to exception handler

---
+++ Climbing.Guide/Climbing.Guide/Exceptions/IExceptionHandler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Climbing.Guide.Exceptions {
+   public interface IExceptionHandler {
+      Task HandleAsync(Exception ex);
+      Task HandleAsync(Exception ex, string message, params object[] messageParameters);
+   }
+}
---
+++ Climbing.Guide/Climbing.Guide/Exceptions/VoidExceptionHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Climbing.Guide.Exceptions {
+   public class VoidExceptionHandler : IExceptionHandler {
+      public Task HandleAsync(Exception ex) {
+         return Task.CompletedTask;
+      }
+
+      public Task HandleAsync(Exception ex, string message, params object[] messageParameters) {
+         return Task.CompletedTask;
+      }
+   }
+}
