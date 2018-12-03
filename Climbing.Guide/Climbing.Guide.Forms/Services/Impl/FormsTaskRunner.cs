22e9d7d5f6eb56fb182fdc9d79a87976956554b5
lyuboasenov <lyubo.asenov@gmail.com>
2018-12-03 23:19:28 +0200

    xamarin forms task runner impl

--- Climbing.Guide/Climbing.Guide.Forms/Services/Impl/FormsTaskRunner.cs
+++ Climbing.Guide/Climbing.Guide.Forms/Services/Impl/FormsTaskRunner.cs
@@ -1,30 +1,28 @@
 ﻿using Climbing.Guide.Logging;
-using Climbing.Guide.Services;
 using Climbing.Guide.Tasks;
 using System;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Climbing.Guide.Forms.Services {
-   public class FormsTaskRunner : TaskRunner {
+   public class FormsTaskRunner : TaskRunner, IMainThreadTaskRunner {
 
-      public FormsTaskRunner(ILogger logger, IErrorService errorService) :base (logger, errorService) {
+      public FormsTaskRunner(ILogger logger, Exceptions.IExceptionHandler exceptionHandler) : 
+         base (logger, exceptionHandler) { }
 
-      }
-
-      public override Task RunOnUIThreadAsync(Action action) {
+      public Task RunOnUIThreadAsync(Action action) {
          Func<Task> asyncAction = async () => { action(); };
 
          return RunOnUIThreadAsync(asyncAction);
       }
 
-      public override Task<TResult> RunOnUIThreadAsync<TResult>(Func<TResult> function) {
+      public Task<TResult> RunOnUIThreadAsync<TResult>(Func<TResult> function) {
          Func<Task<TResult>> asyncFunc = async () => { return function(); };
 
          return RunOnUIThreadAsync(asyncFunc);
       }
 
-      public override Task<TResult> RunOnUIThreadAsync<TResult>(Func<Task<TResult>> function) {
+      public Task<TResult> RunOnUIThreadAsync<TResult>(Func<Task<TResult>> function) {
          Task<TResult> result = null;
          Device.BeginInvokeOnMainThread(() => { result = function(); });
 
