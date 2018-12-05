9f8c6c0b3f1da8d9f07003b9609dd845a93a3899
lyuboasenov <lyubo.asenov@gmail.com>
2018-12-05 18:05:36 +0200

    Add error label and use it

---
+++ Climbing.Guide/Climbing.Guide.Forms/Validations/ErrorLabel.cs
@@ -0,0 +1,26 @@
+﻿using Climbing.Guide.Forms.Validations;
+using Xamarin.Forms;
+
+namespace Climbing.Guide.Forms.Validations {
+   public class ErrorLabel : Label {
+
+      private static ValidationErrorConverter ValidationErrorConverter { get; } = new ValidationErrorConverter();
+
+      public static readonly BindableProperty ErrorKeyProperty =
+         BindableProperty.Create(nameof(ErrorKey), typeof(string), typeof(ErrorLabel), null, propertyChanged: OnErrorKeyPropertyChanged);
+
+      public string ErrorKey {
+         get { return (string)GetValue(ErrorKeyProperty); }
+         set { SetValue(ErrorKeyProperty, value); }
+      }
+
+      private static void OnErrorKeyPropertyChanged(BindableObject bindable, object oldValue, object newValue) {
+         ((ErrorLabel)bindable).OnErrorKeyChanged(bindable, oldValue, newValue);
+      }
+
+      private void OnErrorKeyChanged(BindableObject bindable, object oldValue, object newValue) {
+         var textBinding = new Binding("ValidationErrors", converter: ValidationErrorConverter, converterParameter: newValue);
+         SetBinding(TextProperty, textBinding);
+      }
+   }
+}
