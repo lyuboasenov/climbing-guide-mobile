ebedfc6d8516a3718e24213a053bf278449dae8d
lyuboasenov <lyubo.asenov@gmail.com>
2019-01-02 21:13:40 +0200

    Add Route/Area data template selector

---
+++ Climbing.Guide/Climbing.Guide.Forms/Forms/GuideDataTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using Climbing.Guide.Api.Schemas;
+using System;
+using Xamarin.Forms;
+
+namespace Climbing.Guide.Forms.Forms {
+   public class GuideDataTemplateSelector : DataTemplateSelector {
+
+      public DataTemplate AreaTemplate { get; set; }
+      public DataTemplate RouteTemplate { get; set; }
+
+      protected override DataTemplate OnSelectTemplate(object item, BindableObject container) {
+         DataTemplate template = null;
+         if (item is Area) {
+            template = AreaTemplate;
+         } else if (item is Route) {
+            template = RouteTemplate;
+         } else {
+            throw new ArgumentException($"Unsupported item type {item.GetType()}");
+         }
+
+         return template;
+      }
+   }
+}
