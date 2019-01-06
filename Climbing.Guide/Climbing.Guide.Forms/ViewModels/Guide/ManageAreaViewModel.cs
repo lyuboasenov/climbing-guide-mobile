6df37596d8811545433dcd0190765a1703955de8
lyuboasenov <lyubo.asenov@gmail.com>
2019-01-06 17:52:50 +0200

    Add save area functionality

--- Climbing.Guide/Climbing.Guide.Forms/ViewModels/Guide/ManageAreaViewModel.cs
+++ Climbing.Guide/Climbing.Guide.Forms/ViewModels/Guide/ManageAreaViewModel.cs
@@ -10,6 +10,9 @@ using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using System;
+using Climbing.Guide.Exceptions;
+using Climbing.Guide.Forms.Helpers;
+using Climbing.Guide.Core.Api;
 
 namespace Climbing.Guide.Forms.ViewModels.Guide {
    [PropertyChanged.AddINotifyPropertyChangedInterface]
@@ -20,33 +23,42 @@ namespace Climbing.Guide.Forms.ViewModels.Guide {
       public IDictionary<string, IEnumerable<IRule>> ValidationRules { get; } = new Dictionary<string, IEnumerable<IRule>>();
       public bool IsValid { get; set; }
 
-      public ObservableCollection<Area> Areas { get; set; }
-      public Area SelectedArea { get; set; }
-
       public string Name { get; set; }
       public string Info { get; set; }
       public string Restrictions { get; set; }
-      public string Access { get; set; }
+      public string Approach { get; set; }
       public string Descent { get; set; }
 
       public MapSpan Location { get; set; }
 
+      public ObservableCollection<Area> TraversalPath { get; set; }
+      public Area ParentArea { get; set; }
+
       public ICommand SaveCommand { get; set; }
       public ICommand CancelCommand { get; set; }
 
+      private IApiClient Client { get; }
       private Services.INavigation Navigation { get; }
       private IValidator Validator { get; }
+      private IExceptionHandler Errors { get; }
+      private IAlerts Alerts { get; }
       private bool IsInitialized { get; } = false;
 
       public ManageAreaViewModel(
+         IApiClient client,
          Services.INavigation navigation,
-         IValidator validator) {
+         IValidator validator,
+         IExceptionHandler exceptionHandler,
+         IAlerts alerts) {
+         Client = client;
          Navigation = navigation;
          Validator = validator;
+         Errors = exceptionHandler;
+         Alerts = alerts;
 
          Title = VmTitle;
 
-         Areas = new ObservableCollection<Area>();
+         TraversalPath = new ObservableCollection<Area>();
 
          InitializeValidationRules();
          InitializeCommands();
@@ -54,8 +66,13 @@ namespace Climbing.Guide.Forms.ViewModels.Guide {
          IsInitialized = true;
       }
 
+      public async override Task OnNavigatedToAsync(params object[] parameters) {
+         await base.OnNavigatedToAsync(parameters);
+         await InitializeData(parameters);
+      }
+
       private void InitializeCommands() {
-         SaveCommand = new Command(Save, () => IsValid);
+         SaveCommand = new Command(async () => await Save(), () => IsValid);
          CancelCommand = new Command(async () => await GoBack());
       }
 
@@ -77,6 +94,26 @@ namespace Climbing.Guide.Forms.ViewModels.Guide {
                   Resources.Strings.Guide.Manage_Area_Map_Title)));
       }
 
+      private Task InitializeData(params object[] parameters) {
+         try {
+            var traversalPath = parameters != null && parameters.Length > 0 ? parameters[0] as IEnumerable<Area> : null;
+            if (null != traversalPath) {
+               foreach(var area in traversalPath) {
+                  TraversalPath.Add(area);
+                  ParentArea = area;
+                  if (null != area) {
+                     Location = MapSpan.FromCenterAndRadius(
+                        MapHelper.GetPosition(area.Latitude, area.Longitude),
+                        Distance.FromKilometers(area.Size));
+                  }
+               }
+            }
+            return Task.CompletedTask;
+         } catch (Exception ex) {
+            return Errors.HandleAsync(ex);
+         }
+      }
+
       public void OnPropertyChanged(string propertyName, object before, object after) {
          if (IsInitialized) {
             Validator.Validate(this, propertyName, after);
@@ -88,11 +125,43 @@ namespace Climbing.Guide.Forms.ViewModels.Guide {
       }
 
       private async Task GoBack() {
-         await Navigation.GoBackAsync();
+         await Navigation.GoBackAsync(TraversalPath);
       }
 
-      private void Save() {
+      private async Task Save() {
+         Area area = new Area() {
+            Parent = ParentArea?.Id,
 
+            Name = Name,
+            Info = Info,
+
+            Restrictions = Restrictions,
+            Approach = Approach,
+            Descent = Descent,
+
+            Latitude = (decimal)Location.Center.Latitude,
+            Longitude = (decimal)Location.Center.Longitude,
+            Size = Location.Radius.Kilometers,
+         };
+
+         try {
+            if (null != ParentArea) {
+               await Client.AreasClient.CreateAsync(area, ParentArea.Id.Value);
+            } else {
+               await Client.AreasClient.CreateAsync(area);
+            }
+            await Alerts.DisplayAlertAsync(
+               Resources.Strings.Guide.Manage_Area_Save_Successful_Title,
+               Resources.Strings.Guide.Manage_Area_Save_Successful_Message,
+               Resources.Strings.Main.Ok);
+            await GoBack();
+         } catch (ApiCallException ex) {
+            await Errors.HandleAsync(ex, Resources.Strings.Guide.Manage_Area_Save_Error);
+         } catch (AggregateException ex) {
+            await Errors.HandleAsync(ex, Resources.Strings.Guide.Manage_Area_Save_Error);
+         } catch (Exception ex) {
+            await Errors.HandleAsync(ex, Resources.Strings.Guide.Manage_Area_Save_Error);
+         }
       }
    }
 }
