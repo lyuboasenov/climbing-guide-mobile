27cc574d0170748ebc67d24a4008654954b57311
lyuboasenov <lyubo.asenov@gmail.com>
2019-01-06 14:33:57 +0200

    Minimize base view model navigation coupling

--- Climbing.Guide/Climbing.Guide.Forms/ViewModels/BaseViewModel.cs
+++ Climbing.Guide/Climbing.Guide.Forms/ViewModels/BaseViewModel.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Climbing.Guide.Forms.ViewModels {
    [PropertyChanged.AddINotifyPropertyChangedInterface]
-   public class BaseViewModel : BindableBase, INavigationAware {
+   public class BaseViewModel : BindableBase, INavigatedAware {
       public BaseViewModel Parent { get; set; }
       public bool IsBusy { get; set; }
       public string Title { get; set; }
@@ -13,31 +13,30 @@ namespace Climbing.Guide.Forms.ViewModels {
       public BaseViewModel() {
       }
 
-
-      public void OnNavigatedFrom(INavigationParameters parameters) {
-         OnNavigatedFromAsync(parameters.Select(p => p.Value));
-      }
-
-      public void OnNavigatedTo(INavigationParameters parameters) {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-         OnNavigatedToAsync(parameters.Select(p => p.Value).ToArray());
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-      }
-
-      public void OnNavigatingTo(INavigationParameters parameters) {
-         OnNavigatingToAsync(parameters.Select(p => p.Value));
+      public virtual Task OnNavigatedToAsync(params object[] parameters) {
+         return Task.CompletedTask;
       }
 
       public virtual Task OnNavigatedFromAsync(params object[] parameters) {
          return Task.CompletedTask;
       }
-
-      public virtual Task OnNavigatedToAsync(params object[] parameters) {
-         return Task.CompletedTask;
+      
+      public virtual void OnNavigatedFrom(INavigationParameters parameters) {
+         OnNavigatedFromAsync(parameters.Select(p => p.Value).ToArray()).
+            ContinueWith((task) => {
+               if (task.IsFaulted) {
+                  throw task.Exception;
+               }
+            });
       }
 
-      public virtual Task OnNavigatingToAsync(params object[] parameters) {
-         return Task.CompletedTask;
+      public virtual void OnNavigatedTo(INavigationParameters parameters) {
+         OnNavigatedToAsync(parameters.Select(p => p.Value).ToArray()).
+            ContinueWith((task) => {
+               if (task.IsFaulted) {
+                  throw task.Exception;
+               }
+            });
       }
    }
 }
