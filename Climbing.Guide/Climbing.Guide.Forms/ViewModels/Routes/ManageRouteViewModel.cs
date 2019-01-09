8b7f8f22e999a3c9c856358d4a5b16c844c33c63
lyuboasenov <lyubo.asenov@gmail.com>
2019-01-09 23:11:03 +0200

    Initialize view model

--- Climbing.Guide/Climbing.Guide.Forms/ViewModels/Routes/ManageRouteViewModel.cs
+++ Climbing.Guide/Climbing.Guide.Forms/ViewModels/Routes/ManageRouteViewModel.cs
@@ -9,20 +9,20 @@ using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Climbing.Guide.Forms.Helpers;
+using Climbing.Guide.Forms.Services.GeoLocation;
+using Climbing.Guide.Forms.Validations;
+using Climbing.Guide.Forms.Validations.Rules;
 
 namespace Climbing.Guide.Forms.ViewModels.Routes {
    [PropertyChanged.AddINotifyPropertyChangedInterface]
-   public class ManageRouteViewModel : BaseViewModel {
+   public class ManageRouteViewModel : BaseViewModel, IValidatable {
       public static string VmTitle { get; } = Resources.Strings.Routes.Route_Title;
-
-      private Services.INavigation Navigation { get; }
-      private IExceptionHandler Errors { get; }
 
       public ICommand ViewSchemaCommand { get; set; }
       public ICommand SaveCommand { get; set; }
       public ICommand CancelCommand { get; set; }
-      public string LocalSchemaThumbPath { get; set; }
 
+      public string LocalSchemaThumbPath { get; set; }
       public string Name { get; set; }
       public string Info { get; set; }
       // TODO: fix grades
@@ -32,25 +32,33 @@ namespace Climbing.Guide.Forms.ViewModels.Routes {
       public IEnumerable<string> RouteTypes { get; set; }
       public double Length { get; set; }
       public string FA { get; set; }
-
       public ObservableCollection<Area> TraversalPath { get; set; }
       public Area ParentArea { get; set; }
-
       public ObservableCollection<Point> SchemaRoute { get; set; }
-
       public MapSpan Location { get; set; }
+
+      public IDictionary<string, IEnumerable<string>> ValidationErrors { get; } = new Dictionary<string, IEnumerable<string>>();
+      public IDictionary<string, IEnumerable<IRule>> ValidationRules { get; } = new Dictionary<string, IEnumerable<IRule>>();
+      public bool IsValid { get; set; }
+
+      private Services.Navigation Navigation { get; }
+      private IExceptionHandler Errors { get; }
+      private GeoLocation GeoLocation { get; }
 
       public ManageRouteViewModel(IApiClient client,
          IExceptionHandler errors,
-         Services.INavigation navigation) {
+         Services.Navigation navigation,
+         GeoLocation geoLocation) {
          Navigation = navigation;
          Errors = errors;
+         GeoLocation = geoLocation;
 
          Title = VmTitle;
 
          TraversalPath = new ObservableCollection<Area>();
          SchemaRoute = new ObservableCollection<Point>();
 
+         InitializeValidationRules();
          InitializeCommands();
       }
 
@@ -58,9 +66,27 @@ namespace Climbing.Guide.Forms.ViewModels.Routes {
          await InitializeData(parameters);
       }
 
+      private void InitializeValidationRules() {
+         this.AddRule(nameof(Name),
+            new RequiredRule(
+               string.Format(
+                  Resources.Strings.Main.Validation_Required_Field,
+                  Resources.Strings.Guide.Manage_Area_Name)));
+         this.AddRule(nameof(Info),
+            new RequiredRule(
+               string.Format(
+                  Resources.Strings.Main.Validation_Required_Field,
+                  Resources.Strings.Guide.Manage_Area_Info)));
+         this.AddRule(nameof(Location),
+            new RequiredRule(
+               string.Format(
+                  Resources.Strings.Main.Validation_Required_Field,
+                  Resources.Strings.Guide.Manage_Area_Map_Title)));
+      }
+
       private void InitializeCommands() {
          ViewSchemaCommand = new Command(async () => await ViewSchema());
-         SaveCommand = new Command(Save, CanSave);
+         SaveCommand = new Command(async () => await Save(), () => IsValid);
          CancelCommand = new Command(async () => await GoBack());
       }
 
@@ -68,12 +94,8 @@ namespace Climbing.Guide.Forms.ViewModels.Routes {
          await Navigation.GoBackAsync(TraversalPath);
       }
 
-      private bool CanSave() {
-         return true;
-      }
-
-      private void Save() {
-         
+      private async Task Save() {
+         await Navigation.GoBackAsync(TraversalPath);
       }
 
       private async Task InitializeData(params object[] parameters) {
@@ -102,23 +124,40 @@ namespace Climbing.Guide.Forms.ViewModels.Routes {
                LocalSchemaThumbPath = imagePath;
             }
 
-            LocalSchemaThumbPath = parameters[1] as string;
-
-            using (var exifReader = new ExifLib.ExifReader(LocalSchemaThumbPath)) {
-               double[] latitudeArray, longitudeArray;
-               double latitude, longitude;
-
-               exifReader.GetTagValue(ExifLib.ExifTags.GPSLatitude, out latitudeArray);
-               exifReader.GetTagValue(ExifLib.ExifTags.GPSLongitude, out longitudeArray);
-
-               latitude = latitudeArray[0] + latitudeArray[1] / 60 + latitudeArray[2] / 3600;
-               longitude = longitudeArray[0] + longitudeArray[1] / 60 + longitudeArray[2] / 3600;
-
-               Location = MapSpan.FromCenterAndRadius(new Position(latitude, longitude), new Distance(170));
-            }
+            await InitializeLocationAsync();
          }catch(Exception ex) {
             await Errors.HandleAsync(ex);
          }
+      }
+
+      private async Task InitializeLocationAsync() {
+         try {
+            await InitializeLocationFromExifAsync();
+         } catch {
+            await InitializeLocationWithCurrentAsync();
+         }
+      }
+
+      private Task InitializeLocationFromExifAsync() {
+         using (var exifReader = new ExifLib.ExifReader(LocalSchemaThumbPath)) {
+            double[] latitudeArray, longitudeArray;
+            double latitude, longitude;
+
+            exifReader.GetTagValue(ExifLib.ExifTags.GPSLatitude, out latitudeArray);
+            exifReader.GetTagValue(ExifLib.ExifTags.GPSLongitude, out longitudeArray);
+
+            latitude = latitudeArray[0] + latitudeArray[1] / 60 + latitudeArray[2] / 3600;
+            longitude = longitudeArray[0] + longitudeArray[1] / 60 + longitudeArray[2] / 3600;
+
+            Location = MapSpan.FromCenterAndRadius(new Position(latitude, longitude), new Distance(170));
+         }
+
+         return Task.CompletedTask;
+      }
+
+      private async Task InitializeLocationWithCurrentAsync() {
+         var location = await GeoLocation.GetCurrentOrDefaultAsync();
+         Location = MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), new Distance(170));
       }
 
       private Task ViewSchema() {
